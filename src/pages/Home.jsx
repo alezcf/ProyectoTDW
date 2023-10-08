5fdeecb6e61bbabc1763abfbf3a568127182f1c2
alezcf <ialeczander@gmail.com>
2023-10-07 22:47:22 -0300

    add panel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,60 @@
-import Dog from '/src/pages/components/Dog.jsx'
+import React, { useState } from 'react';
+import Dog from '/src/pages/components/Dog.jsx';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
+  const [value, setValue] = useState(1); // Establecer el valor inicial para mostrar la pestaña "Item Two"
 
-  
-  console.log("Llegada al Home")
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    return (
-            <div>
-              <Dog></Dog>
-            </div>
-    );
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Candidato" />
+          <Tab label="Aceptados" />
+          <Tab label="Rechazados" />
+        </Tabs>
+      </Box>
+      {/* Contenido de la pestaña "Item One" (vacío) */}
+      <CustomTabPanel value={value} index={0}>
+        <Dog />
+      </CustomTabPanel>
+      {/* Contenido de la pestaña "Item Two" (con el componente Dog) */}
+      <CustomTabPanel value={value} index={1}>
+        {/* Contenido vacío */}
+      </CustomTabPanel>
+      {/* Contenido de la pestaña "Item Three" (vacío) */}
+      <CustomTabPanel value={value} index={2}>
+        {/* Contenido vacío */}
+      </CustomTabPanel>
+    </Box>
+  );
 };
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography component={'span'}>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default Home;
--- src/pages/components/Dog.jsx
+++ src/pages/components/Dog.jsx
@@ -3,13 +3,13 @@ import { React, useEffect, useState } from "react";
 import { fetchRandomDog } from "../../../queries/query";
 
 export default function Dog() {
-
-    const [dogImage, setDogImage] = useState('');
+    const [dogData, setDogData] = useState({ name: '', image: '' });
 
     useEffect(() => {
         fetchRandomDog()
         .then((imageURL) => {
-            setDogImage(imageURL);
+          const dogName = generateDogName(); // Generar un nombre para el perro
+          setDogData({ name: dogName, image: imageURL }); // Guardar el nombre y la URL de la imagen en el estado
         })
         .catch((error) => {
             console.error('Error al obtener la foto del perro:', error);
@@ -17,5 +17,20 @@ export default function Dog() {
         });
     }, []);
 
-    return <img src={dogImage} alt="Perro" />;
+    const generateDogName = () => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let dogName = '';
+        for (let i = 0; i < 6; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            dogName += characters.charAt(randomIndex);
+        }
+        return dogName;
+    };
+
+    return (
+        <div>
+            <h2>{dogData.name}</h2>
+            <img src={dogData.image} alt="Perro" />
+        </div>
+    );
 }
