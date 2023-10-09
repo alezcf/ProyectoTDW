cef8d621fd2f16d5ff9202cead01c709624c9e8d
SrTimin <matias.bravo2102@alumnos.ubiobio.cl>
2023-10-08 22:31:42 -0300

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,52 @@
-import React, { useState } from 'react';
-import Dog from '/src/pages/components/Dog.jsx';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+// En el componente Home
+import React, { useState } from "react";
+import Dog from "/src/pages/components/Dog.jsx";
+import { Grid, Button } from "@mui/material";
 
 const Home = () => {
-  const [value, setValue] = useState(1); // Establecer el valor inicial para mostrar la pestaña "Item Two"
+  const [acceptedDogs, setAcceptedDogs] = useState([]);
+  const [rejectedDogs, setRejectedDogs] = useState([]);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const handleAccept = (dog) => {
+    setAcceptedDogs((prevAcceptedDogs) => [...prevAcceptedDogs, dog]);
+    
+  };
+
+  const handleReject = (dog) => {
+    setRejectedDogs((prevRejectedDogs) => [...prevRejectedDogs, dog]);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Candidato" />
-          <Tab label="Aceptados" />
-          <Tab label="Rechazados" />
-        </Tabs>
-      </Box>
-      {/* Contenido de la pestaña "Item One" (vacío) */}
-      <CustomTabPanel value={value} index={0}>
-        <Dog />
-      </CustomTabPanel>
-      {/* Contenido de la pestaña "Item Two" (con el componente Dog) */}
-      <CustomTabPanel value={value} index={1}>
-        {/* Contenido vacío */}
-      </CustomTabPanel>
-      {/* Contenido de la pestaña "Item Three" (vacío) */}
-      <CustomTabPanel value={value} index={2}>
-        {/* Contenido vacío */}
-      </CustomTabPanel>
-    </Box>
+    <Grid container spacing={10} style={{ width: "100vw" }}>
+      <Grid item xs={4}>
+        {/* Contenido del perro candidato */}
+        <h2>Perro Candidato</h2>
+        <div style={{ maxWidth: "100%", overflow: "hidden" }}>
+          <Dog onAccept={handleAccept} onReject={handleReject} />
+        </div>
+      </Grid>
+      <Grid item xs={4}>
+        {/* Contenido de perros aceptados */}
+        <h2>Perros Aceptados</h2>
+        {acceptedDogs.map((dog, index) => (
+          <div key={index}>
+            <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
+            <p>{dog.name}</p>
+          </div>
+        ))}
+      </Grid>
+      <Grid item xs={4}>
+        {/* Contenido de perros rechazados */}
+        <h2>Perros Rechazados</h2>
+        {rejectedDogs.map((dog, index) => (
+          <div key={index}>
+            <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
+            <p>{dog.name}</p>
+          </div>
+        ))}
+      </Grid>
+    </Grid>
   );
 };
-
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography component={'span'}>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
 
 export default Home;
--- src/pages/components/Dog.jsx
+++ src/pages/components/Dog.jsx
@@ -1,36 +1,60 @@
-
-import { React, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchRandomDog } from "../../../queries/query";
 
-export default function Dog() {
-    const [dogData, setDogData] = useState({ name: '', image: '' });
+export default function Dog({ onAccept, onReject }) {
+  const [dogData, setDogData] = useState({ name: "", image: "" });
 
-    useEffect(() => {
-        fetchRandomDog()
-        .then((imageURL) => {
-          const dogName = generateDogName(); // Generar un nombre para el perro
-          setDogData({ name: dogName, image: imageURL }); // Guardar el nombre y la URL de la imagen en el estado
-        })
-        .catch((error) => {
-            console.error('Error al obtener la foto del perro:', error);
-          // Manejo del error, por ejemplo, mostrar un mensaje al usuario
-        });
-    }, []);
+  useEffect(() => {
+    fetchRandomDog()
+      .then((imageURL) => {
+        const dogName = generateDogName();
+        setDogData({ name: dogName, image: imageURL });
+      })
+      .catch((error) => {
+        console.error("Error al obtener la foto del perro:", error);
+      });
+  }, []);
 
-    const generateDogName = () => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        let dogName = '';
-        for (let i = 0; i < 6; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            dogName += characters.charAt(randomIndex);
-        }
-        return dogName;
-    };
+  const generateDogName = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let dogName = "";
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      dogName += characters.charAt(randomIndex);
+    }
+    return dogName;
+  };
 
-    return (
-        <div>
-            <h2>{dogData.name}</h2>
-            <img src={dogData.image} alt="Perro" />
-        </div>
-    );
+  const handleAccept = () => {
+    onAccept({ name: dogData.name, image: dogData.image });
+    fetchRandomDog()
+      .then((imageURL) => {
+        const dogName = generateDogName();
+        setDogData({ name: dogName, image: imageURL });
+      })
+      .catch((error) => {
+        console.error("Error al obtener la foto del perro:", error);
+      });
+  };
+
+  const handleReject = () => {
+    onReject({ name: dogData.name, image: dogData.image });
+    fetchRandomDog()
+      .then((imageURL) => {
+        const dogName = generateDogName();
+        setDogData({ name: dogName, image: imageURL });
+      })
+      .catch((error) => {
+        console.error("Error al obtener la foto del perro:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Nombre: {dogData.name}</h2>
+      <img src={dogData.image} alt="Perro" />
+      <button onClick={handleAccept}>Aceptar</button>
+      <button onClick={handleReject}>Rechazar</button>
+    </div>
+  );
 }
