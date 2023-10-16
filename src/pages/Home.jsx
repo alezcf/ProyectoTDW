603eea128ab973d06e74ef57f5c445396c9afdb8
SrTimin <matias.bravo2102@alumnos.ubiobio.cl>
2023-10-16 17:34:17 -0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// En el componente Home
 import React, { useState } from "react";
 import Dog from "/src/pages/components/Dog.jsx";
 import { Grid, Button } from "@mui/material";
@@ -8,42 +7,93 @@ const Home = () => {
   const [rejectedDogs, setRejectedDogs] = useState([]);
 
   const handleAccept = (dog) => {
-    setAcceptedDogs((prevAcceptedDogs) => [...prevAcceptedDogs, dog]);
-    
+    setAcceptedDogs((prevAcceptedDogs) => [...prevAcceptedDogs, {...dog, showDescription: false}]);
   };
 
   const handleReject = (dog) => {
-    setRejectedDogs((prevRejectedDogs) => [...prevRejectedDogs, dog]);
+    setRejectedDogs((prevRejectedDogs) => [...prevRejectedDogs, {...dog, showDescription: false}]);
+  };
+
+  const handleToggleDescription = (dog, setDogs) => {
+    setDogs((prevDogs) => prevDogs.map((d) => d === dog ? {...d, showDescription: !d.showDescription} : d));
+  };
+
+  const handleRepent = (dog, setFromDogs, setToDogs) => {
+    setFromDogs((prevFromDogs) => prevFromDogs.filter((d) => d !== dog));
+    setToDogs((prevToDogs) => [...prevToDogs, dog]);
+  };
+
+  const handleRemove = (dog, setFromDogs) => {
+    setFromDogs((prevFromDogs) => prevFromDogs.filter((d) => d !== dog));
   };
 
   return (
     <Grid container spacing={10} style={{ width: "100vw" }}>
       <Grid item xs={4}>
-        {/* Contenido del perro candidato */}
         <h2>Perro Candidato</h2>
         <div style={{ maxWidth: "100%", overflow: "hidden" }}>
           <Dog onAccept={handleAccept} onReject={handleReject} />
         </div>
       </Grid>
       <Grid item xs={4}>
-        {/* Contenido de perros aceptados */}
         <h2>Perros Aceptados</h2>
-        {acceptedDogs.map((dog, index) => (
-          <div key={index}>
-            <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
-            <p>{dog.name}</p>
-          </div>
-        ))}
+        <div style={{ maxHeight: "400px", overflowY: "scroll" }}>
+          {acceptedDogs.map((dog, index) => (
+            <div key={index}>
+              <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
+              <p>{dog.name}</p>
+              {dog.showDescription && <p>{dog.description}</p>}
+              <Button 
+                variant="contained" 
+                onClick={() => handleToggleDescription(dog, setAcceptedDogs)}
+              >
+                {dog.showDescription ? "Ocultar Descripción" : "Mostrar Descripción"}
+              </Button><br/>
+              <Button 
+                variant="contained" 
+                onClick={() => handleRepent(dog, setAcceptedDogs, setRejectedDogs)}
+              >
+                Arrepentirse
+              </Button><br/>
+              <Button 
+                variant="contained" 
+                onClick={() => handleRemove(dog, setAcceptedDogs)}
+              >
+                Eliminar
+              </Button>
+            </div>
+          ))}
+        </div>
       </Grid>
       <Grid item xs={4}>
-        {/* Contenido de perros rechazados */}
         <h2>Perros Rechazados</h2>
-        {rejectedDogs.map((dog, index) => (
-          <div key={index}>
-            <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
-            <p>{dog.name}</p>
-          </div>
-        ))}
+        <div style={{ maxHeight: "400px", overflowY: "scroll" }}>
+          {rejectedDogs.map((dog, index) => (
+            <div key={index}>
+              <img src={dog.image} alt="Perro" style={{ maxWidth: "100%" }} />
+              <p>{dog.name}</p>
+              {dog.showDescription && <p>{dog.description}</p>}
+              <Button 
+                variant="contained" 
+                onClick={() => handleToggleDescription(dog, setRejectedDogs)}
+              >
+                {dog.showDescription ? "Ocultar Descripción" : "Mostrar Descripción"}
+              </Button><br/>
+              <Button 
+                variant="contained" 
+                onClick={() => handleRepent(dog, setRejectedDogs, setAcceptedDogs)}
+              >
+                Arrepentirse
+              </Button><br/>
+              <Button 
+                variant="contained" 
+                onClick={() => handleRemove(dog, setRejectedDogs)}
+              >
+                Eliminar
+              </Button>
+            </div>
+          ))}
+        </div>
       </Grid>
     </Grid>
   );
--- src/pages/components/Dog.jsx
+++ src/pages/components/Dog.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { fetchRandomDog } from "../../../queries/query";
+import React from 'react';
+import { useQuery } from 'react-query';
+import { CircularProgress } from '@mui/material';
+import { fetchRandomDog } from '../../../queries/query';
+import { loremIpsum } from 'lorem-ipsum';
 
 export default function Dog({ onAccept, onReject }) {
-  const [dogData, setDogData] = useState({ name: "", image: "" });
-
-  useEffect(() => {
-    fetchRandomDog()
-      .then((imageURL) => {
-        const dogName = generateDogName();
-        setDogData({ name: dogName, image: imageURL });
-      })
-      .catch((error) => {
-        console.error("Error al obtener la foto del perro:", error);
-      });
-  }, []);
-
   const generateDogName = () => {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let dogName = "";
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let dogName = '';
     for (let i = 0; i < 6; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       dogName += characters.charAt(randomIndex);
@@ -25,36 +15,51 @@ export default function Dog({ onAccept, onReject }) {
     return dogName;
   };
 
+  const generateDogDescription = () => {
+    return loremIpsum({
+      count: 1, // Número de párrafos que deseas generar
+      format: 'plain', // Formato del texto generado (puede ser 'plain' o 'html')
+      random: Math.random, // Función para generar números aleatorios
+    });
+  };
+
+  const { data: imageURL, isFetching, refetch } = useQuery('fetchDog', fetchRandomDog, {
+    onSuccess: () => {
+      setDogData({ name: generateDogName(), image: imageURL, description: generateDogDescription() });
+    },
+  });
+
+  const [dogData, setDogData] = React.useState({ name: '', image: '', description: '' });
+
   const handleAccept = () => {
-    onAccept({ name: dogData.name, image: dogData.image });
-    fetchRandomDog()
-      .then((imageURL) => {
-        const dogName = generateDogName();
-        setDogData({ name: dogName, image: imageURL });
-      })
-      .catch((error) => {
-        console.error("Error al obtener la foto del perro:", error);
-      });
+    onAccept({ name: dogData.name, image: dogData.image, description: dogData.description });
+    refetch();
   };
 
   const handleReject = () => {
-    onReject({ name: dogData.name, image: dogData.image });
-    fetchRandomDog()
-      .then((imageURL) => {
-        const dogName = generateDogName();
-        setDogData({ name: dogName, image: imageURL });
-      })
-      .catch((error) => {
-        console.error("Error al obtener la foto del perro:", error);
-      });
+    onReject({ name: dogData.name, image: dogData.image, description: dogData.description });
+    refetch();
   };
 
   return (
     <div>
-      <h2>Nombre: {dogData.name}</h2>
-      <img src={dogData.image} alt="Perro" />
-      <button onClick={handleAccept}>Aceptar</button>
-      <button onClick={handleReject}>Rechazar</button>
+      {isFetching ? ( // Muestra el loader si isFetching es true
+        <div>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div>
+          <h2>Nombre: {dogData.name}</h2>
+          <img src={dogData.image} alt='Perro' />
+          <p>{dogData.description}</p>
+          <button onClick={handleAccept} disabled={isFetching}>
+            Aceptar
+          </button>
+          <button onClick={handleReject} disabled={isFetching}>
+            Rechazar
+          </button>
+        </div>
+      )}
     </div>
   );
 }
